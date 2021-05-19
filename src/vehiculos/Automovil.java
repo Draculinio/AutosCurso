87a8655aaa962bb176143fcf591162fced0d30e3
Sebastian Scheib <stackpointerex@gmail.com>
2021-05-19 17:51:16 -0300

    Clase de constructores

--- src/vehiculos/Automovil.java
+++ src/vehiculos/Automovil.java
@@ -5,9 +5,26 @@ public class Automovil {
 	private String modelo;
 	private int velocidadMaxima;
 	private int velocidadActual=0;
-	private boolean encendido;
+	private boolean encendido= false;
 	private String patente;
 	private String color;
+	
+	public Automovil(String marca,String modelo,int velocidadMaxima,String patente,String color) {
+		this.marca = marca;
+		this.setModelo(modelo);
+		this.velocidadMaxima = velocidadMaxima;
+		this.setPatente(patente);
+		this.setColor(color);
+	}
+	
+	public Automovil(String marca,String modelo, int velocidadMaxima, String patente) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.velocidadMaxima = velocidadMaxima;
+		this.patente = patente;
+		this.color = "Marron";
+		
+	}
 	
 	public void setMarca(String m) {
 		marca = m;
@@ -29,6 +46,7 @@ public class Automovil {
 	public void frenar(int velocidad) {
 		//TODO: Implementar el freno
 		//HOLA Gsdfddffsdlfjfkdsfkjsdhfdjkslfhdjklfhkljsfdahfdkasl
+		//TODO:fkdfjslkfjdlkfsdjkl
 	}
 	
 	public void encender() {
@@ -37,6 +55,38 @@ public class Automovil {
 	
 	public void apagar() {
 		
+	}
+
+	public int getVelocidadMaxima() {
+		return velocidadMaxima;
+	}
+
+	public void setVelocidadMaxima(int velocidadMaxima) {
+		this.velocidadMaxima = velocidadMaxima;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
 	}
 
 	

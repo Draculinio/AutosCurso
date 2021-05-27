cebfbf0a5ab80e4b4f1ea6741d34dd7ce0d0429c
Sofia Maccarone <sofiamaccarone@gmail.com>
2021-05-27 10:41:47 -0300

    Probando cambios

--- src/vehiculos/Bicicleta.java
+++ src/vehiculos/Bicicleta.java
@@ -1,5 +1,17 @@
 package vehiculos;
 
 public class Bicicleta {
+	
+	private String marca;
+	private String modelo;
+	private String color;
+	private boolean plegable;
+	
+	public bicicleta(String marca, String modelo, String color, boolean plegable) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.color = color;
+		this.plegable = plegable;
+	}
 
 }
--- src/vehiculos/Camion.java
+++ src/vehiculos/Camion.java
@@ -1,5 +1,22 @@
 package vehiculos;
 
 public class Camion {
+	
+	private String marca;
+	private String modelo;
+	private String color;
+	private int capacidadCarga;
+	private String patente;
+	private int velocidadMaxima;
+	private int velocidadActual;
+	private boolean encendido;
+	
+	public camion (String marca, String modelo, String color, int capacidadCarga, String patente, int velocidadMaxima) {
+		this.marca= marca;
+		this.modelo = modelo;
+		this.color = color;
+		this.capacidadCarca = capacidadCarga;
+		this.velocidadMaxima = velocidadMaxima;
+	}
 
 }
